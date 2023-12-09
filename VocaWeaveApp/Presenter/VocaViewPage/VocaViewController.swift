74af21d477d2e5a462523489140ae0af08153dd0
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-12-09 23:17:58 +0900

    VocaVIewPage UI 완료

--- VocaWeaveApp/Presenter/VocaViewPage/VocaViewController.swift
+++ VocaWeaveApp/Presenter/VocaViewPage/VocaViewController.swift
@@ -8,7 +8,79 @@
 import UIKit
 
 class VocaViewController: UIViewController {
+    // MARK: - Property
+    let vocaView = VocaView()
+    lazy var plusButton = UIBarButtonItem(image: UIImage(systemName: "plus"),
+                                     style: .plain,
+                                     target: self,
+                                     action: #selector(plustButtonAction))
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNav()
+        configureUI()
+        setup()
+    }
+
+    // MARK: - Helper
+    private func configureNav() {
+        let titleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "단어장"
+            label.textColor = .black
+            label.font = .boldSystemFont(ofSize: 32)
+            return label
+        }()
+        let titleItem = UIBarButtonItem(customView: titleLabel)
+        navigationItem.leftBarButtonItem = titleItem
+        navigationItem.rightBarButtonItem = plusButton
+        navigationController?.configureBasicAppearance()
+    }
+
+    private func configureUI() {
+        let defaultValue = 8
+        view.addSubview(vocaView)
+
+        vocaView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(defaultValue * 2)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+    }
+
+    private func setup() {
+        vocaView.vocaTableView.dataSource = self
+        vocaView.vocaTableView.register(
+                                VocaTableViewCell.self,
+                                forCellReuseIdentifier: VocaTableViewCell.identifier)
+        vocaView.vocaSegmentedControl.selectedSegmentIndex = 0
+        vocaView.vocaSegmentedControl.addTarget(self,
+                                                action: #selector(vocaSegmentedControlValueChanged),
+                                                for: .valueChanged)
+    }
+
+    // MARK: - Action
+    @objc private func plustButtonAction() {
+        print("플러스 버튼")
+    }
+
+    @objc private func vocaSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+        let selectedSegmentIndex = sender.selectedSegmentIndex
+        print("Selected Segment Index: \(selectedSegmentIndex)")
+    }
+}
+
+extension VocaViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+                                    withIdentifier: VocaTableViewCell.identifier,
+                                    for: indexPath)
+                            as? VocaTableViewCell else { return UITableViewCell()}
+        cell.bindingData()
+        return cell
     }
 }
