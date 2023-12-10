789096e8c3cd5225ea5fb0bed11b5212d8e06c01
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-12-10 18:51:40 +0900

    categorywordspage View 구현

---
+++ VocaWeaveApp/Presenter/CategoryWordspage/CagtegoryCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  CagtegoryCollectionViewCell.swift
+//  VocaWeaveApp
+//
+//  Created by 천광조 on 12/10/23.
+//
+
+import UIKit
+import SnapKit
+
+class CagtegoryCollectionViewCell: UICollectionViewCell {
+    static let identifier = "CagtegoryCollectionViewCell"
+
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .black
+        return label
+    }()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.backgroundColor = .yellow
+        contentView.layer.cornerRadius = 15
+
+        contentView.addSubview(categoryLabel)
+        categoryLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+}
